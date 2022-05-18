f5a8336e380d1e1e7e3a91935984bee4ee9c9175
Juan Ignacio Bernardelli <juan.bernardelli@globant.com>
2022-05-18 18:06:44 -0300

    Modify files #Day12

--- go/src/Hello/main.go
+++ go/src/Hello/main.go
@@ -3,11 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var challenge = "#90DaysOfDevOps"
-	const daystotal = 90
-	var dayscomplete = 11
 
-	fmt.Println("Welcome to " + challenge)
-	fmt.Println("This is a", daystotal, "challenge and you have completed", dayscomplete, "days")
-	fmt.Println("Great work")
+	challenge := "#90DaysOfDevOps"
+	const daysTotal int = 90
+	var remainingDays uint = 90
+
+	fmt.Printf("Welcome to %v\n challenge.\nThis challenge consists of %v days\n", challenge, daysTotal)
+
+	var twitterName string
+	var daysCompleted uint
+	// asking for user input
+
+	fmt.Println("Enter Your Twitter Handle: ")
+	fmt.Scanln(&twitterName)
+
+	fmt.Println("How many days have you completed?: ")
+	fmt.Scanln(&daysCompleted)
+
+	// calculate remaining days
+	remainingDays = remainingDays - daysCompleted
+
+	fmt.Printf("Thank you %v for taking part and completing %v days.\n", twitterName, daysCompleted)
+	fmt.Printf("You have %v days remaining for the %v challenge\n", remainingDays, challenge)
+	fmt.Println("Good luck")
 }
